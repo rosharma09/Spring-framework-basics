4e4d930ef055d7693575124d76981e45aa2f972a
rosharma09 <56192256+rosharma09@users.noreply.github.com>
2021-03-10 01:09:37 +0530

    New files added for the @Component annotation example

---
+++ src/main/java/com/qa/demo/spring_framework_basics/MobilePhone.java
@@ -0,0 +1,11 @@
+package com.qa.demo.spring_framework_basics;
+
+public interface MobilePhone {
+
+	String category = "Mobile Phones";
+
+	void call(int callNumber);
+
+	int dailNumber(String number);
+
+}
---
+++ src/main/java/com/qa/demo/spring_framework_basics/Samsung.java
@@ -0,0 +1,17 @@
+package com.qa.demo.spring_framework_basics;
+
+public class Samsung implements MobilePhone {
+
+	private String mobNumber;
+
+	public void call(int number) {
+		System.out.println("Calling the dailed Number: " + String.valueOf(number));
+		dailNumber(String.valueOf(number));
+	}
+
+	public int dailNumber(String number) {
+		System.out.println("Dailing the number: "+Integer.parseInt(number));
+		return Integer.parseInt(number);
+	}
+
+}
